250a2d81868e5033d28b24b8357f768a9b50a84f
akky25 <ry.akita1991@gmail.com>
2021-01-27 22:49:12 +0900

    add AllCharacters

--- cp10/src/components/pages/Characters.tsx
+++ cp10/src/components/pages/Characters.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
-// import { Redirect, Route, Switch, RouteComponentProps } from 'react-router';
-// import { Divider } from 'semantic-ui-react';
+import { Redirect, Route, Switch, RouteComponentProps } from 'react-router';
+import { Divider } from 'semantic-ui-react';
 
 import HomeButton from 'containers/molecules/HomeButton';
+import AllCharacters from 'containers/templates/AllCharacters';
 
-const Characters: FC = () => (
+const Characters: FC<RouteComponentProps> = ({ match }) => (
   <>
+    <header>
+      <h1>『SLAM DUNK』登場人物</h1>
+    </header>
+    <Switch>
+      <Route exact paht={`${match.path}`}>
+        <AllCharacters />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+    <Divider hidden />
     <HomeButton />
   </>
 );
