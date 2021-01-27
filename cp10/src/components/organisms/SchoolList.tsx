6651c05a87fa9013cde5777259ad397b37b0f801
akky25 <ry.akita1991@gmail.com>
2021-01-27 18:19:49 +0900

    add SchoolList

---
+++ cp10/src/components/organisms/SchoolList.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { List } from 'semantic-ui-react';
+
+type School = {
+  code: string;
+  name: string;
+};
+
+const SchooList: FC<{ schools: School[] }> = ({ schools }) => (
+  <>
+    <h2>登場人物</h2>
+    <List as="ul">
+      {schools.map((school) => (
+        <List.Item as="li" key={school.code}>
+          <Link to={`/characters/${school.code}`}>{school.name}</Link>
+          {/* <Link to="/characters">{school.name}</Link> */}
+        </List.Item>
+      ))}
+      <List.Item as="li" key="all">
+        <Link to="/characters">全校の選手（身長順）</Link>
+      </List.Item>
+    </List>
+  </>
+);
+
+export default SchooList;
