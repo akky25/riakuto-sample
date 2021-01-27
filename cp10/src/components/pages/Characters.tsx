f6c57d20b1d46717460d351339342dd990bafaab
akky25 <ry.akita1991@gmail.com>
2021-01-27 23:39:41 +0900

    add SchoolCharacters

--- cp10/src/components/pages/Characters.tsx
+++ cp10/src/components/pages/Characters.tsx
@@ -4,6 +4,7 @@ import { Divider } from 'semantic-ui-react';
 
 import HomeButton from 'containers/molecules/HomeButton';
 import AllCharacters from 'containers/templates/AllCharacters';
+import SchoolCharacters from 'containers/templates/SchoolCharacters';
 
 const Characters: FC<RouteComponentProps> = ({ match }) => (
   <>
@@ -11,8 +12,11 @@ const Characters: FC<RouteComponentProps> = ({ match }) => (
       <h1>『SLAM DUNK』登場人物</h1>
     </header>
     <Switch>
-      <Route exact paht={`${match.path}`}>
+      <Route exact path={`${match.path}`}>
         <AllCharacters />
+      </Route>
+      <Route path={`${match.path}/:schoolCode`}>
+        <SchoolCharacters />
       </Route>
       <Redirect to="/" />
     </Switch>
