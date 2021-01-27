9b065881e3831bb6ad07b1a9055cc5a3b783470e
akky25 <ry.akita1991@gmail.com>
2021-01-27 21:36:45 +0900

    add homebutton

---
+++ cp10/src/components/pages/Characters.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+// import { Redirect, Route, Switch, RouteComponentProps } from 'react-router';
+// import { Divider } from 'semantic-ui-react';
+
+import HomeButton from 'containers/molecules/HomeButton';
+
+const Characters: FC = () => (
+  <>
+    <HomeButton />
+  </>
+);
+
+export default Characters;
