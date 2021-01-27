4b6e2d9bd1f76a3e3e961ba65a5dbd355b11ab9f
akky25 <ry.akita1991@gmail.com>
2021-01-27 16:17:33 +0900

    modify index.tsx App.tsx App.tsx

--- cp10/src/App.tsx
+++ cp10/src/App.tsx
@@ -1,24 +1,30 @@
-import React, { FC } from 'react';
-import logo from './logo.svg';
+import { FC, useEffect } from 'react';
+import { Redirect, Route, Switch, useHistory, useLocation } from 'react-router';
+
+import Home from 'components/pages/Home';
+import Characters from 'components/pages/Characters';
 import './App.css';
 
-const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App: FC = () => {
+  const { hash, pathname } = useLocation();
+  const { action } = useHistory();
 
+  useEffect(() => {
+    if (!hash || action !== 'POP') {
+      window.scrollTo(0, 0);
+    }
+  }, [action, hash, pathname]);
+
+  return (
+    <div className="container">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/characters" component={Characters} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
+};
 export default App;
--- cp10/src/index.tsx
+++ cp10/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
